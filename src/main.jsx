8ecabcc601523c934d66edeab2f7260c4dd0c0e4
Natécia Costa <natecia_ilha@hotmail.com>
2026-03-05 16:50:45 -0300

    fix build

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import TaskPage from './pages/TaskPage.jsx'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/task',
+    element: <TaskPage />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/task" element={<TaskPage />} />
-      </Routes>
-    </HashRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 )
