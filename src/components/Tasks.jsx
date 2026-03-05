8616864aa87dba1e11a51de08f85684d917b7175
Natécia Costa <natecia_ilha@hotmail.com>
2026-03-05 16:15:44 -0300

    novo projeto

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,26 @@
+import {CheckIcon, ChevronRightIcon, Trash2} from "lucide-react"
+import { useNavigate } from "react-router-dom"
+
+function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+    const navigate = useNavigate()
+
+    function onSeeDetailsClick(tasks) {
+        const query = new URLSearchParams()
+        query.set('title', tasks.title)
+        query.set('description', tasks.description)
+        navigate(`/task?${query.toString()}`)
+    }
+
+    return (
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+        {tasks.map((tasks) => (
+        <li key={tasks.id} className="flex gap-2">
+            <button onClick={() => onTaskClick(tasks.id)} className={`bg-slate-400 text-left w-full flex items-center gap-2 text-white p-2 rounded-md ${tasks.isCompleted && "line-through"}`}>{tasks.isCompleted && <CheckIcon />}{tasks.title}</button>
+            <button onClick={() => onSeeDetailsClick(tasks)} className="bg-slate-400 text-white p-2 rounded-md"><ChevronRightIcon /></button>
+            <button onClick={() => onDeleteTaskClick(tasks.id)} className="bg-slate-400 text-white p-2 rounded-md"><Trash2 /></button>
+        </li>
+        ))}
+    </ul>
+)}
+
+export default Tasks
